7070eb52547e0fd718b210151d40edb96edb1cc1
ruben <rubenlorenzo85@yahoo.es>
2021-02-25 13:01:53 +0100

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Header from './components/Header'
+import './App.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div id="content">
+
+      </div>
+      <Footer/>
     </div>
   );
 }
