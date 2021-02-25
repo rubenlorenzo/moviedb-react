0a1dc40016827e1b2b321f70de948188b2e89ca3
rubenlorenzo <rubenlorenzo85@yahoo.es>
2021-02-25 18:13:53 +0100

    add boostrap and add Navbar to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import  { Navbar, Nav } from 'react-bootstrap';
 import './Header.scss'
 
 function Header() {
-  return(
-    <div id="header">
-      <h1>Moviedb-React</h1>
-    </div>
-  )
+  return (
+    <Navbar>
+      <Navbar.Brand href="#home">Moviedb-React</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home"></Nav.Link>
+          <Nav.Link href="#link"></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Header;
